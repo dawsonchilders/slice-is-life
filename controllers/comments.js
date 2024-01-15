22e34bb10d5438ff36f47dc27e84ac1dfc344f95
dawsonchilders <dawsonchilders@gmail.com>
2024-01-14 18:45:21 -0800

    fixes comments not posting

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,8 +1,22 @@
+const Post = require('../models/post');
 const Comment = require('../models/comment');
 
 module.exports = {
   create,
 };
+
+// async function create(req, res) {
+//   req.body.user = req.user._id;
+//   req.body.userName = req.user.name;
+//   req.body.userAvatar = req.user.avatar;
+//   req.body.post = req.params.id;
+//   try {
+//     await Comment.create(req.body);
+//     res.redirect('/posts/home');
+//   } catch (err) {
+//     console.log(err)
+//   }
+// }
 
 async function create(req, res) {
   req.body.user = req.user._id;
@@ -10,12 +24,11 @@ async function create(req, res) {
   req.body.userAvatar = req.user.avatar;
   req.body.post = req.params.id;
   try {
-    await Comment.create(req.body);
+    const comment = await Comment.create(req.body);
+    await Post.findByIdAndUpdate(req.body.post, { $push: { comments: comment._id } });
     res.redirect('/posts/home');
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 }
-
-
 
